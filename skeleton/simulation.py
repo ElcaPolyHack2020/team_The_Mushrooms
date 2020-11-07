e3215c8260f444c6ba6ffc2a4ba88633b73befeb
kmohit <kmohit@student.ethz.ch>
2020-11-07 18:24:07 +0100

    ari branch

--- skeleton/simulation.py
+++ skeleton/simulation.py
@@ -11,6 +11,13 @@ class Simulation:
         self.bus_depot_start_edge = bus_depot_start_edge
         self.bus_depot_end_edge = bus_depot_end_edge
 
+    def append_edges(self, cur_rou, e_fr, e_to, bus_id):
+        add_rou = traci.simulation.findRoute(e_fr, e_to).edges
+        if len(cur_rou) != 0:
+            cur_rou.pop()
+        cur_rou.extend(add_rou)
+        traci.vehicle.setRoute(bus_id, cur_rou)
+        
     def run(self):
         # Create a bus for the persons
         bus_index = 0
@@ -19,16 +26,30 @@ class Simulation:
             bus_index += 1
 
             try:
-                traci.vehicle.add(vehID=bus_id, typeID="BUS_S", routeID="", depart=person.depart + 240.0, departPos=0, departSpeed=0, departLane=0, personCapacity=4)
+                traci.vehicle.add(vehID=bus_id, typeID="BUS_S", routeID="", depart=person.depart + 0.0, departPos=0, departSpeed=0, departLane=0, personCapacity=4)
                 traci.vehicle.setRoute(bus_id, [self.bus_depot_start_edge])
                 
-                traci.vehicle.changeTarget(bus_id, person.edge_from)
-                traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_from, pos=person.position_from, laneIndex=0, duration=50, flags=tc.STOP_DEFAULT)
+                #traci.vehicle.changeTarget(bus_id, self.bus_depot_start_edge)
+                
+                #traci.vehicle.changeTarget(bus_id, person.edge_from)
+                #traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_from, pos=person.position_from, laneIndex=0, duration=50, flags=tc.STOP_DEFAULT)
                 
 
-                traci.vehicle.setRoute(bus_id, [person.edge_from])
-                traci.vehicle.changeTarget(bus_id, person.edge_to)
+                #traci.vehicle.setRoute(bus_id, [self.bus_depot_start_edge, person.edge_from, person.edge_to])
+                #traci.vehicle.changeTarget(bus_id, person.edge_from)
+                #traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_from, pos=person.position_from, laneIndex=0, duration=50, flags=tc.STOP_DEFAULT)
+                traci.vehicle.changeTarget(bus_id, person.edge_from)
+                o_r = list(traci.vehicle.getRoute(bus_id))
+                #print(o_r)
+                #sleep(3)
+                traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_from, pos=person.position_from, laneIndex=0, duration=50, flags=tc.STOP_DEFAULT)
+
+                # create fake vehicle
+                self.append_edges(o_r, person.edge_from, person.edge_to, bus_id)
+
                 traci.vehicle.setStop(vehID=bus_id, edgeID=person.edge_to, pos=person.position_to, laneIndex=0, duration=50, flags=tc.STOP_DEFAULT)
+
+                self.append_edges(o_r, person.edge_to, self.bus_depot_end_edge, bus_id)
 
             except traci.exceptions.TraCIException as err:
                 print("TraCIException: {0}".format(err))
@@ -40,7 +61,7 @@ class Simulation:
         step = 0
         while step <= self.simulation_steps:
             traci.simulationStep()
-            if self.sleep_time > 0: 
+            if self.sleep_time > 0:
                 sleep(self.sleep_time)
             step += 1
             #print(traci.vehicle.getSubscriptionResults('bus_0'))
